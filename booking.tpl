ab73f3ce9e32e88ec2e74223925fffde4b2c6203
c4po187 <c4po187@gmail.com>
2015-10-12 06:38:04 +0100

    CHANGELOG
    - Added validation to the booking form, including regular expressions.

--- booking.tpl
+++ booking.tpl
@@ -1,70 +1,88 @@
 {include file="header.tpl" title="Bookings" page_style="style/booking.css"}
+		{literal}<script>
+			function isReqChecked() {
+				var checks = document.getElementsByClassName('chk');
+				var isChecked = false;
+				for (var i = 0; i < checks.length; ++i) {
+					if (checks[i].checked) {
+						isChecked = true;
+						break;
+					}
+				}
+				if (!isChecked) {
+					$('#booking_form').submit(function(e) { e.preventDefault(); });
+					alert("You must select at least one requirement!");
+				} else {
+					$('#booking_form').unbind('submit').submit();
+				}
+			}
+		</script>{/literal}
 		<div class="centralize" id="container">
 			<h1>Booking Form</h1>
 			<div id="form_wrapper">
-				<form method="POST" action="thanks.php">
+				<form id="booking_form" method="POST" action="thanks.php">
 					<table>
 						<tr>
 							<td class="tlabel"><b>First Name<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="first_name" required="required" oninvalid="this.setCustomValidity('First name is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="first_name" autofocus required pattern="([A-Z][a-z]{1,25}(\s|-)?){1,5}" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}
 							<td class="tlabel"><b>Requirements<span style="color:red;">*</span></b></td>
 							<td class="tlabel"><label>Windows<input class="chk" type="checkbox" name="req[]" value="Window"/></label></td>
 							<td class="color_choices">{include file="color_select.tpl" color_s="color_windows"}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Surname<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="surname" required="required" oninvalid="this.setCustomValidity('Surname is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="surname" required pattern="([A-Z][a-z]{1,25}-?){1,2}" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}</td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Doors<input class="chk" type="checkbox" name="req[]" value="Door" /></label></td>
 							<td class="color_choices">{include file="color_select.tpl" color_s="color_doors"}</</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>House Name</b></td>
-							<td class="entries"><input type="text" name="house_name"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="house_name" pattern="([A-Za-z]\s?-?)+" />{/literal}</td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Conservatories<input class="chk" type="checkbox" name="req[]" value="Conservatory" /></label></td>
 							<td class="color_choices">{include file="color_select.tpl" color_s="color_conservatories"}</</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Address (Line 1)<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="addr_line1" required="required" oninvalid="this.setCustomValidity('Address is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="addr_line1" required pattern="^\d+\s([A-Z][a-z]{1,50}\s?){1,}$" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}</td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Soffits<input class="chk" type="checkbox" name="req[]" value="Soffit" /></label></td>
 							<td class="color_choices">{include file="color_select.tpl" color_s="color_soffits"}</</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Address (Line 2)</b></td>
-							<td class="entries"><input type="text" name="addr_line2"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="addr_line2" pattern="([A-Za-z]\s?-?)+" />{/literal}</td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Fascias<input class="chk" type="checkbox" name="req[]" value="Fascia" /></label></td>
 							<td class="color_choices">{include file="color_select.tpl" color_s="color_fascias"}</</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Town/City<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="town_city" required="required" oninvalid="this.setCustomValidity('Town or city is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="town_city" required="required" pattern="([A-Z][a-z]{1,25}(\s|-)?){1,4}" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>County<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="county" required="required" oninvalid="this.setCustomValidity('County is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="county" required pattern="([A-Z][a-z]{1,25}\s?){1,3}" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Postcode<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="postcode" required="required" oninvalid="this.setCustomValidity('Postcode is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="postcode" required pattern="^[A-Z]{1,2}\d{1,2}\s\d[A-Z]{2}$" onchange="try{setCustomValidity('')}catch(e){}" />{/literal}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Phone</b></td>
-							<td class="entries"><input type="text" name="phone"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="phone" pattern="^0\d{2,5}\s?\d{6,8}$" />{/literal}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Mobile<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="mobile" required="required" oninvalid="this.setCustomValidity('Contact method is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries">{literal}<input class="i_fields" type="text" name="mobile" required pattern="^07\d{3}\s?\d{6}$" onchange="try{this.setCustomValidity('')}catch(e){}" />{/literal}</td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Email</b></td>
-							<td class="entries"><input type="text" name="email"></td>
+							<td class="entries"><input class="i_fields" type="email" name="email"></td>
 						</tr>
 						<tr id="book_btn_cell">
-							<td colspan="5"><input id="book_btn" type="submit" value="Book"></td>
+							<td colspan="5"><input id="book_btn" type="submit" value="Book" onclick="isReqChecked();" /></td>
 						</tr>
 					</table>
 					<pre id="form_mandatory"><b><i>Fields marked with '<span style="color:red;">*</span>' are mandatory.</i></b></pre>
--- templates_c/91634e0e0c01c10e6e14c3e708435c3d839c19c3_0.file.booking.tpl.php
+++ templates_c/91634e0e0c01c10e6e14c3e708435c3d839c19c3_0.file.booking.tpl.php
@@ -1,16 +1,16 @@
-<?php /* Smarty version 3.1.28-dev/63, created on 2015-10-11 02:39:43
+<?php /* Smarty version 3.1.28-dev/63, created on 2015-10-12 07:22:30
          compiled from "C:\xampp\htdocs\workspace\thermalmax\booking.tpl" */ ?>
 <?php
 $_valid = $_smarty_tpl->decodeProperties(array (
   'has_nocache_code' => false,
   'version' => '3.1.28-dev/63',
-  'unifunc' => 'content_5619afcf1d76f5_27384413',
+  'unifunc' => 'content_561b439706acf8_60889640',
   'file_dependency' => 
   array (
     '91634e0e0c01c10e6e14c3e708435c3d839c19c3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\workspace\\thermalmax\\booking.tpl',
-      1 => 1444523982,
+      1 => 1444627346,
       2 => 'file',
     ),
   ),
@@ -21,19 +21,39 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'file:footer.tpl' => 1,
   ),
 ),false);
-if ($_valid && !is_callable('content_5619afcf1d76f5_27384413')) {
-function content_5619afcf1d76f5_27384413 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_561b439706acf8_60889640')) {
+function content_561b439706acf8_60889640 ($_smarty_tpl) {
 $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('title'=>"Bookings",'page_style'=>"style/booking.css"), 0, false)->render();
 ?>
 
+		<?php echo '<script'; ?>
+>
+			function isReqChecked() {
+				var checks = document.getElementsByClassName('chk');
+				var isChecked = false;
+				for (var i = 0; i < checks.length; ++i) {
+					if (checks[i].checked) {
+						isChecked = true;
+						break;
+					}
+				}
+				if (!isChecked) {
+					$('#booking_form').submit(function(e) { e.preventDefault(); });
+					alert("You must select at least one requirement!");
+				} else {
+					$('#booking_form').unbind('submit').submit();
+				}
+			}
+		<?php echo '</script'; ?>
+>
 		<div class="centralize" id="container">
 			<h1>Booking Form</h1>
 			<div id="form_wrapper">
-				<form method="POST" action="thanks.php">
+				<form id="booking_form" method="POST" action="thanks.php">
 					<table>
 						<tr>
 							<td class="tlabel"><b>First Name<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="first_name" required="required" oninvalid="this.setCustomValidity('First name is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="first_name" autofocus required pattern="([A-Z][a-z]{1,25}(\s|-)?){1,5}" onchange="try{setCustomValidity('')}catch(e){}" />
 							<td class="tlabel"><b>Requirements<span style="color:red;">*</span></b></td>
 							<td class="tlabel"><label>Windows<input class="chk" type="checkbox" name="req[]" value="Window"/></label></td>
 							<td class="color_choices"><?php $_smarty_tpl->setupSubTemplate('file:color_select.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('color_s'=>"color_windows"), 0, false)->render();
@@ -42,7 +62,7 @@ $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smar
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Surname<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="surname" required="required" oninvalid="this.setCustomValidity('Surname is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="surname" required pattern="([A-Z][a-z]{1,25}-?){1,2}" onchange="try{setCustomValidity('')}catch(e){}" /></td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Doors<input class="chk" type="checkbox" name="req[]" value="Door" /></label></td>
 							<td class="color_choices"><?php $_smarty_tpl->setupSubTemplate('file:color_select.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('color_s'=>"color_doors"), 0, true)->render();
@@ -51,7 +71,7 @@ $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smar
 						</tr>
 						<tr>
 							<td class="tlabel"><b>House Name</b></td>
-							<td class="entries"><input type="text" name="house_name"></td>
+							<td class="entries"><input class="i_fields" type="text" name="house_name" pattern="([A-Za-z]\s?-?)+" /></td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Conservatories<input class="chk" type="checkbox" name="req[]" value="Conservatory" /></label></td>
 							<td class="color_choices"><?php $_smarty_tpl->setupSubTemplate('file:color_select.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('color_s'=>"color_conservatories"), 0, true)->render();
@@ -60,7 +80,7 @@ $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smar
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Address (Line 1)<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="addr_line1" required="required" oninvalid="this.setCustomValidity('Address is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="addr_line1" required pattern="^\d+\s([A-Z][a-z]{1,50}\s?){1,}$" onchange="try{setCustomValidity('')}catch(e){}" /></td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Soffits<input class="chk" type="checkbox" name="req[]" value="Soffit" /></label></td>
 							<td class="color_choices"><?php $_smarty_tpl->setupSubTemplate('file:color_select.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('color_s'=>"color_soffits"), 0, true)->render();
@@ -69,7 +89,7 @@ $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smar
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Address (Line 2)</b></td>
-							<td class="entries"><input type="text" name="addr_line2"></td>
+							<td class="entries"><input class="i_fields" type="text" name="addr_line2" pattern="([A-Za-z]\s?-?)+" /></td>
 							<td class="tlabel"></td>
 							<td class="tlabel"><label>Fascias<input class="chk" type="checkbox" name="req[]" value="Fascia" /></label></td>
 							<td class="color_choices"><?php $_smarty_tpl->setupSubTemplate('file:color_select.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('color_s'=>"color_fascias"), 0, true)->render();
@@ -78,30 +98,30 @@ $_smarty_tpl->setupSubTemplate('file:header.tpl', $_smarty_tpl->cache_id, $_smar
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Town/City<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="town_city" required="required" oninvalid="this.setCustomValidity('Town or city is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="town_city" required="required" pattern="([A-Z][a-z]{1,25}(\s|-)?){1,4}" onchange="try{setCustomValidity('')}catch(e){}" /></td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>County<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="county" required="required" oninvalid="this.setCustomValidity('County is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="county" required pattern="([A-Z][a-z]{1,25}\s?){1,3}" onchange="try{setCustomValidity('')}catch(e){}" /></td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Postcode<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="postcode" required="required" oninvalid="this.setCustomValidity('Postcode is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="postcode" required pattern="^[A-Z]{1,2}\d{1,2}\s\d[A-Z]{2}$" onchange="try{setCustomValidity('')}catch(e){}" /></td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Phone</b></td>
-							<td class="entries"><input type="text" name="phone"></td>
+							<td class="entries"><input class="i_fields" type="text" name="phone" pattern="^0\d{2,5}\s?\d{6,8}$" /></td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Mobile<span style="color:red;">*</span></b></td>
-							<td class="entries"><input type="text" name="mobile" required="required" oninvalid="this.setCustomValidity('Contact method is required!')" oninput="setCustomValidity('')"></td>
+							<td class="entries"><input class="i_fields" type="text" name="mobile" required pattern="^07\d{3}\s?\d{6}$" onchange="try{this.setCustomValidity('')}catch(e){}" /></td>
 						</tr>
 						<tr>
 							<td class="tlabel"><b>Email</b></td>
-							<td class="entries"><input type="text" name="email"></td>
+							<td class="entries"><input class="i_fields" type="email" name="email"></td>
 						</tr>
 						<tr id="book_btn_cell">
-							<td colspan="5"><input id="book_btn" type="submit" value="Book"></td>
+							<td colspan="5"><input id="book_btn" type="submit" value="Book" onclick="isReqChecked();" /></td>
 						</tr>
 					</table>
 					<pre id="form_mandatory"><b><i>Fields marked with '<span style="color:red;">*</span>' are mandatory.</i></b></pre>
